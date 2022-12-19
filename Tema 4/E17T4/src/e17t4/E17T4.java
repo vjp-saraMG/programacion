c7d5a1bf845298f2524f342e6a00343db3d89439
Sara Mahía Gómez <114755244+vjp-saraMG@users.noreply.github.com>
2022-12-19 20:55:01 +0100

    Tema 4

---
+++ Tema 4/E17T4/src/e17t4/E17T4.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package e17t4;
+import java.util.Scanner;
+/**
+ *
+ * @author smahiag01
+ */
+public class E17T4 {
+    
+    public static char pedirLetra(){
+        Scanner entrada = new Scanner(System.in);
+        char letra;
+        System.out.println("Introduce una letra: ");
+        letra = entrada.nextLine().charAt(0);
+        return letra;
+    }
+    
+    public static int generarLetraAleatoria(){
+        double aleatorio = Math.random()*(122-97) + 97;
+        int letraAleatoriaEntera = (int) aleatorio;
+        return letraAleatoriaEntera;
+    }
+    
+    public static int comprobarResultado(char letraUsuario, char letraAleatoriaEntera){
+        letraAleatoriaEntera = (char) generarLetraAleatoria();
+        //letraUsuario = (char) letraAleatoriaEntera;
+        //System.out.println(letraAleatoriaEntera);
+        if (letraUsuario == letraAleatoriaEntera){
+            System.out.println("¡Enhorabuena! Has acertado.");
+        }
+        else{
+            if (letraUsuario < letraAleatoriaEntera){
+                System.out.println("La letra que tienes que adivinar está antes que la que intentas adivinar.");
+            }
+            else{
+                System.out.println("La letra que tienes que adivinar está después que la que tienes que adivinar.");
+            }
+        }
+        return letraAleatoriaEntera;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char letraUsuario;
+        char letraAleatoria = (char) generarLetraAleatoria();
+        int contador = 0;
+        
+        do{
+            letraUsuario = pedirLetra();
+            comprobarResultado(letraUsuario, letraAleatoria);
+            contador = contador + 1;
+        }while(letraUsuario != letraAleatoria);
+        
+        System.out.println("El numero de intentos que has necesitado es de: " + contador);
+        
+        
+    }
+    
+}
